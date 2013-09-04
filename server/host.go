3013049518969696946ba21a72db35ce25ba27fc
mdennebaum <matt@ninjacipher.com>
2013-09-03 21:49:04 -0400

    enabled response header addition

--- server/host.go
+++ server/host.go
@@ -10,14 +10,15 @@ import (
 )
 
 type Host struct {
-	conf          *util.DynMap
-	url           string
-	port          string
-	root          string
-	static        string
-	proxies       []*httputil.ReverseProxy
-	backends      *util.DynMap
-	proxyChannel  chan *httputil.ReverseProxy
+	conf         *util.DynMap
+	url          string
+	port         string
+	root         string
+	static       string
+	headers      *util.DynMap
+	proxies      []*httputil.ReverseProxy
+	backends     *util.DynMap
+	proxyChannel chan *httputil.ReverseProxy
 }
 
 func NewHost(config *util.DynMap) *Host {
@@ -33,6 +34,7 @@ func (this *Host) Init() {
 	this.root, _ = this.conf.GetString("root")
 	this.static, _ = this.conf.GetString("static")
 	this.backends, _ = this.conf.GetDynMap("backends")
+	this.headers, _ = this.conf.GetDynMap("headers")
 	this.initLog()
 	this.initBackends()
 	this.initHostHandler()
@@ -47,43 +49,55 @@ func (this *Host) initLog() {
 	// }
 }
 
-func (this *Host) addHeaders(w http.ResponseWriter) {
-
+func (this *Host) addHeaders(w http.ResponseWriter) http.ResponseWriter {
+	if this.headers != nil {
+		for k, v := range this.headers.Map {
+			w.Header().Set(k, v.(string))
+		}
+	}
+	return w
 }
 
+//configure our reverse proxy backends
 func (this *Host) initBackends() {
+
+	//check if we have any backends to proxy requests to
 	if this.backends != nil {
+
+		//loop over configs
 		for _, v := range this.backends.Map {
+			//grab the server url and parse it
 			serverUrl, err := url.Parse(v.(string))
+
+			//if its not a valid url log the error
 			if err != nil {
 				log.Println(err)
 			} else {
+				//create a new proxy for our backend and add it to our proxy slice
 				this.proxies = append(this.proxies, httputil.NewSingleHostReverseProxy(serverUrl))
 			}
 		}
-	}
-	
-	go func(){
-		for {
-			for p := range this.proxies {
-		    		this.proxyChannel <- p
-		  	}
-		}
+
+		//loop over proxies and block till one is needed
+		go func() {
+			for {
+				for _, p := range this.proxies {
+					this.proxyChannel <- p
+				}
+			}
+		}()
 	}
 }
 
-func (this *Host) Next() *httputil.ReverseProxy{
-  return <- this.proxyChannel
-}
-
-//grab the next round robin backend to handle req
-func (this *Host) getProxyBackend() *httputil.ReverseProxy {
-	return this.proxies[this.proxyPosition.Next()]
+//get the next available proxy from our round robin channel
+func (this *Host) NextProxy() *httputil.ReverseProxy {
+	return <-this.proxyChannel
 }
 
 // Provide proxying of a url. Reverse proxy just masks the path
 func (this *Host) initHostHandler() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(this.url+":"+this.port+"/", func(w http.ResponseWriter, r *http.Request) {
+		w = this.addHeaders(w)
 		if this.static != "" {
 			if strings.HasPrefix(r.URL.Path, this.static) {
 				http.ServeFile(w, r, this.root+r.URL.Path)
@@ -91,7 +105,7 @@ func (this *Host) initHostHandler() {
 			}
 		}
 		//get next proxy
-		rewriteProxy := this.Next()
+		rewriteProxy := this.NextProxy()
 		r.RequestURI = strings.Replace(r.RequestURI, "/", "", 1)
 		r.URL.Path = strings.Replace(r.URL.Path, "/", "", 1)
 		rewriteProxy.ServeHTTP(w, r)
